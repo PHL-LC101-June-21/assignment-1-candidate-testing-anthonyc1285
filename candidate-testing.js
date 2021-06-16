7c77ade52e9462adff976364e872f37505082b39
ACirio1285 <anthony.cirio@gmail.com>
2021-06-16 15:29:37 +0000

    looped questions for askQuestion(). grade percentage for gradeQuiz()

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,8 +8,8 @@ let candidateName = "";
 let question;
 let correctAnswer;
 let candidateAnswer;
-let questions = [];
-let correctAnswers = ["Sally Ride"];
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["sally ride", "true", "40", "trajectory", "3"];
 let candidateAnswers = [];
 
 
@@ -22,13 +22,19 @@ const input = require('readline-sync');
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
+
+  for(let i = 0; i < 5; i++) {
+  candidateAnswers.push(input.question(questions[i]).toLowerCase());
+}
+
+console.log(candidateAnswers);
 
 
-console.log(question);
-const x = require('readline-sync');
-let candidateAnswer = candidateAnswers.push(x.question("Enter answer: "));
+// let question = "Who was the first American woman in space? ";
+// let correctAnswer = "Sally Ride";
+// console.log(question);
+// const x = require('readline-sync');
+// let candidateAnswer = candidateAnswers.push(x.question("Enter answer: "));
 
 }
 
@@ -36,15 +42,17 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-  let grade = "";
-  if (candidateAnswers[0].toLowerCase() === correctAnswers[0].toLowerCase()){
-    grade = "Correct!";
+  let grade = 0;
+for(let i =0; i < 5; i++){
+  
+  if(correctAnswers[i] === candidateAnswers[i]) {
+    grade += 1;
   }
-  else {
-    grade = "Wrong";
-  }
-  //console.log(grade);
-  return grade;
+}
+
+//console.log((grade/5 * 100) + "%");
+
+return grade/5 * 100;
   
 }
 
