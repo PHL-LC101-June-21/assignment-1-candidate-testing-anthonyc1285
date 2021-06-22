f6b9c5168631a1d080076651d7ef8b7008b02f6f
ACirio1285 <anthony.cirio@gmail.com>
2021-06-22 00:53:42 +0000

    Formatting attempt Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,7 +6,7 @@ const input = require('readline-sync');
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
-let correctAnswer;
+let correctAnswer = "Sally Ride";
 let candidateAnswer = " ";
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
@@ -32,7 +32,7 @@ function askQuestion() {
 
 
 // let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
+//let correctAnswer = "Sally Ride";
 // console.log(question);
 // const x = require('readline-sync');
 // let candidateAnswer = candidateAnswers.push(x.question("Enter answer: "));
