99f16062f3a1e252865296114d29f8737ddbdca8
ACirio1285 <anthony.cirio@gmail.com>
2021-06-22 00:50:59 +0000

    formatting changes for GitHub Classroom

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,11 +5,11 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
+let question = "Who was the first American woman in space? ";
 let correctAnswer;
-let candidateAnswer;
-let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
-let correctAnswers = ["sally ride", "true", "40", "trajectory", "3"];
+let candidateAnswer = " ";
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
 
@@ -32,7 +32,7 @@ function askQuestion() {
 
 
 // let question = "Who was the first American woman in space? ";
-// let correctAnswer = "Sally Ride";
+let correctAnswer = "Sally Ride";
 // console.log(question);
 // const x = require('readline-sync');
 // let candidateAnswer = candidateAnswers.push(x.question("Enter answer: "));
@@ -46,7 +46,7 @@ function gradeQuiz(candidateAnswers) {
   let grade = 0;
 for(let i =0; i < 5; i++){
   
-  if(correctAnswers[i] === candidateAnswers[i]) {
+  if(correctAnswers[i].toLowerCase() === candidateAnswers[i]) {
     grade += 1;
   }
 }
